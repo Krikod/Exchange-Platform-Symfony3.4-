8ccfc905ea3f83c8378a96b7b4aac435006f47ca
Krikod <kristen.kod@gmail.com>
2019-05-14 15:42:06 +0200

    imbrication form multiple Advert / Categories

--- src/AppBundle/Form/AdvertType.php
+++ src/AppBundle/Form/AdvertType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -26,7 +27,12 @@ class AdvertType extends AbstractType
             ->add('published', CheckboxType::class, array(
                 'required' => false))
             ->add('image', ImageType::class)// Champ imbriqué image
-        ->add('save', SubmitType::class)
+            ->add('categories', CollectionType::class, array(
+                'entry_type' => CategoryType::class,
+                'allow_add' => true,
+                'allow_delete' => true
+            ))
+            ->add('save', SubmitType::class)
         ;
     }
 
